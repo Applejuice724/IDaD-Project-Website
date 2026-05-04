3e57ed5e66dc32d72952018043c7a0a974c8052d
Apple Babble <jeremy.grassi@gmail.com>
2026-05-04 17:05:59 +1000

    Removed confusing text
    I was very sleepy when I did this section, removed and replaced

--- Interface and Design Assignment/src/components/Footer.vue
+++ Interface and Design Assignment/src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
         <h2>Welcome to our card shop!</h2>
-        <p>Discover a wide variety of cards for all occasions. Whether you're looking for birthday cards, thank you cards, or just a special card to brighten someone's day, we've got you covered. Browse our collection and find the perfect card for your needs!</p>
+        <p>Thank you for visiting our website. We hope you find the perfect conversation about cards for all your playing needs!</p>
     </div>
 </template>
