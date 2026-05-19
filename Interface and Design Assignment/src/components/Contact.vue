d03016b7ab03c9b57f8c3ea6bbf065cc0b7e1f74
Alexi-105350153 <105350153@student.swin.edu.au>
2026-05-19 17:52:28 +1000

    Add Contact.vue input fields

--- Interface and Design Assignment/src/components/Contact.vue
+++ Interface and Design Assignment/src/components/Contact.vue
@@ -1,3 +1,16 @@
 <template>
     <p> Contact template</p>
+
+    <fieldset>
+        <label for="name">Your Name:</label>
+        <input type="text" id="name" name="name" pattern="[A-Za-z]+" required>
+        <label for="email"> Your Email:</label>
+        <input type="text" id="email" name="email" pattern="[\w\.]+@[A-Za-z.]+" required>
+        <label for="subject"> Email Subject:</label>
+        <input type="text" id="subject" name="subject" required>
+        <label for="message">Your message:</label>
+        <input type="textarea" id="message" name="message" required>
+    </fieldset>
+    <button type="button" onclick="alert('Email sent successfully')">Send email</button>
+
 </template>
