011e43e17dd844eed55e68ab226683e4c023fb10
Alexi-105350153 <105350153@student.swin.edu.au>
2026-05-25 11:05:05 +1000

    Improve Contact.vue UI

--- Interface and Design Assignment/src/components/Contact.vue
+++ Interface and Design Assignment/src/components/Contact.vue
@@ -4,13 +4,21 @@
     <fieldset>
         <label for="name">Your Name:</label>
         <input type="text" id="name" name="name" pattern="[A-Za-z]+" required>
+        <br>
         <label for="email"> Your Email:</label>
         <input type="text" id="email" name="email" pattern="[\w\.]+@[A-Za-z.]+" required>
+        <br>
         <label for="subject"> Email Subject:</label>
         <input type="text" id="subject" name="subject" required>
-        <label for="message">Your message:</label>
-        <input type="textarea" id="message" name="message" required>
+        <br>
+        <p><label for="message">Your message:</label></p>
+        <textarea id="message" name="message" rows="4" cols="50"required></textarea>
     </fieldset>
     <button type="button" onclick="alert('Email sent successfully')">Send email</button>
 
 </template>
+<style>
+input{
+    margin: 10px;
+}
+</style>
