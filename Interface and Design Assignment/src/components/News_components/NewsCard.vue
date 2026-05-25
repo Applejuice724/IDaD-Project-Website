e6174e05767643120cb884bad7219101975db6af
samsonswinburne <161039641+samsonswinburne@users.noreply.github.com>
2026-05-25 22:13:04 +1000

    Add min and max height to text properties, will need to be updated later to be responsive

--- Interface and Design Assignment/src/components/News_components/NewsCard.vue
+++ Interface and Design Assignment/src/components/News_components/NewsCard.vue
@@ -15,7 +15,9 @@ defineProps({
     <div class="p-3 rounded shadow border" role="button">
       <img :src="newsItem.image_url" alt="News Image" class="object-fit-scale border rounded mw-100"/>
       <h2>{{ newsItem.title }}</h2>
-      <p>{{ newsItem.short_description }}</p>
+      <p class="card-text" style="max-height: 100px; min-height: 100px; overflow: hidden;">
+        {{ newsItem.short_description }}
+      </p>
     </div>
   </router-link>
 </template>
