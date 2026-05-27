c5cfbef35ebd47de1ab1ebdc1962db61aa175406
Alexi-105350153 <105350153@student.swin.edu.au>
2026-05-27 22:50:34 +1000

    Fix links to be router rather than a href

--- Interface and Design Assignment/src/components/Footer.vue
+++ Interface and Design Assignment/src/components/Footer.vue
@@ -9,31 +9,31 @@
                 <div class="col-12 col-md-3 mb-3 mb-md-0">
                     <h5 class="mb-3">{{ globals.websiteName }}</h5>
                     <ul class="list-unstyled">
-                        <li><a href="/about" class="text-decoration-none text-muted">About Us</a></li>
-                        <li><a href="/events" class="text-decoration-none text-muted">Events</a></li>
-                        <li><a href="/contact" class="text-decoration-none text-muted">Contact</a></li>
+                        <li><router-link to="/about" class="text-decoration-none text-muted" >About Us</router-link></li>
+                        <li><router-link to="/events" class="text-decoration-none text-muted">Events</router-link></li>
+                        <li><router-link to="/contact" class="text-decoration-none text-muted">Contact</router-link></li>
                     </ul>
                 </div>
                 <div class="col-12 col-md-3 mb-3 mb-md-0">
                     <h5 class="mb-3">Community</h5>
                     <ul class="list-unstyled">
-                        <li><a href="/news" class="text-decoration-none text-muted">News</a></li>
-                        <li><a href="/deckbuilder" class="text-decoration-none text-muted">Deck Builder</a></li>                        
+                        <li><router-link to="/news" class="text-decoration-none text-muted">News</router-link></li>
+                        <li><router-link to="/deckbuilder" class="text-decoration-none text-muted">Deck Builder</router-link></li>                        
                     </ul>
                 </div>
                 <div class="col-12 col-md-3 mb-3 mb-md-0">
                     <h5 class="mb-3">Resources</h5>
                     <ul class="list-unstyled">                        
-                        <li><a href="/guides" class="text-decoration-none text-muted">Strategy Guides</a></li>
+                        <li><router-link to="/guides" class="text-decoration-none text-muted">Strategy Guides</router-link></li>
 
-                        <li><a href="/faq" class="text-decoration-none text-muted">FAQ</a></li>
+                        <li><router-link to="/faq" class="text-decoration-none text-muted">FAQ</router-link></li>
                     </ul>
                 </div>
                 <div class="col-12 col-md-3">
                     <h5 class="mb-3">Connect</h5>
                     <ul class="list-unstyled">
-                        <li><a href="#" class="text-decoration-none text-muted">Twitter</a></li>
-                        <li><a href="#" class="text-decoration-none text-muted">Discord</a></li>
+                        <li><router-link to="#" class="text-decoration-none text-muted">Twitter</router-link></li>
+                        <li><router-link to="#" class="text-decoration-none text-muted">Discord</router-link></li>
                     </ul>
                 </div>
             </div>
